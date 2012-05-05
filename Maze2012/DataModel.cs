0316aa2d3f52d52c56e85f5783bfa638c4291b8e
unknown <dean@dean-laptop-w7.(none)>
2012-05-06 00:17:19 +0100

    Test commit

---
+++ Maze2012/DataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maze2012
+{
+    class DataModel
+    {
+        MazeStructure mazeStructure;
+
+        public DataModel()
+        {
+            mazeStructure = new MazeStructure();
+        }
+    }
+}
---
+++ Maze2012/MazeStructure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Maze2012
+{
+    class MazeStructure
+    {
+        List<Cell> cells = new List<Cell>();
+
+        public MazeStructure()
+        {
+            if (cells != null)
+            {
+                cells.Clear();
+            }
+
+
+        }
+    }
+}
