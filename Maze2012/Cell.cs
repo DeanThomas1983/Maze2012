dd5ed32f853c3294c159b77aeb71ac5f8d689756
DeanThomas1983 <dean.plymouth@googlemail.com>
2012-05-06 01:47:28 +0100

    Begin to build maze building algorithms

--- Maze2012/Cell.cs
+++ Maze2012/Cell.cs
@@ -2,10 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 
 namespace Maze2012
 {
     class Cell
     {
+#region PRIVATE_VARIABLES
+        //  Lookup table for connected cells
+        private Cell cellToNorth;
+        private Cell cellToSouth;
+        private Cell cellToEast;
+        private Cell cellToWest;
+        //  Walls between cells
+        private Boolean northWall;
+        private Boolean southWall;
+        private Boolean eastWall;
+        private Boolean westWall;
+#endregion
+        public Cell CellToNorth { get { return cellToNorth; } set { cellToNorth = value; } }
+        public Cell CellToSouth { get { return cellToSouth; } set { cellToSouth = value; } }
+        public Cell CellToEast { get { return cellToEast; } set { cellToEast = value; } }
+        public Cell CellToWest { get { return cellToWest; } set { cellToWest = value; } }
+        public int NumberOfWalls { get { return countWalls(); } }
+
+        public Cell demolishRandomWall()
+        {
+            //  get a list of connected cells from the current cell
+            List<Cell> connectedCells;
+
+
+            Cell result;
+
+            Random random = new Random();
+
+            
+
+            return result;
+        }
+
+        private Cell demolishNorthWall()
+        {
+            this.northWall = false;
+            this.cellToNorth.southWall = false;
+
+            return cellToNorth;
+        }
+
+        private Cell demolishSouthWall()
+        {
+            this.southWall = false;
+            this.cellToSouth.northWall = false;
+
+            return cellToSouth;
+        }
+
+        private Cell demolishEastWall()
+        {
+            this.eastWall = false;
+            this.cellToEast.westWall = false;
+
+            return cellToEast;
+        }
+
+        private Cell demolishWestWall()
+        {
+            this.westWall = false;
+            this.cellToWest.eastWall = false;
+
+            return cellToWest;
+        }
+
+        private int countWalls()
+        {
+            int result = 0;
+
+            if (northWall)
+                result++;
+
+            if (southWall)
+                result++;
+
+            if (westWall)
+                result++;
+
+            if (eastWall)
+                result++;
+
+            return result;
+        }
+
+        public Cell()
+        {
+            northWall = true;
+            southWall = true;
+            eastWall = true;
+            westWall = true;
+        }
     }
 }
--- Maze2012/MazeStructure.cs
+++ Maze2012/MazeStructure.cs
@@ -7,16 +7,101 @@ namespace Maze2012
 {
     class MazeStructure
     {
+#region PRIVATE_VARIABLES
         List<Cell> cells = new List<Cell>();
 
-        public MazeStructure()
+        //  Dimensions in cells
+        int width;
+        int height;
+        //  Cell sizes
+        int cellWidth;
+        int cellHeight;
+#endregion
+
+        public MazeStructure() : this(8, 8) { }
+        
+        public MazeStructure(int width, int height)
         {
+            //  Clear any previous structures
             if (cells != null)
             {
                 cells.Clear();
             }
 
+            //  Create the array of cells (as a 1D list)
+            for (int i = 0; i < width * height; i++)
+            {
+                Cell newCell = new Cell();
 
+                cells.Add(newCell);
+            }
+
+            //  Connect the cells
+            connectCells();
+
+            //  Set class variables
+            this.width = width;
+            this.height = height;
+        }
+
+        public void generateMaze()
+        {
+            //  Later will add additional algorithms
+            this.generateDepthFirst();
+        }
+
+        private void connectCells()
+        {
+            for (int i = 0; i < cells.Count; i++)
+            {
+                int row = 0;
+                int col = 0;
+
+                //  Take the 1D number and conver to a 2D position
+                if (col == width)
+                {
+                    row++;
+                    col = 0;
+                }
+
+                //  Set up connections
+                if (row > 0)
+                    cells[i].CellToNorth = cells[((col - 1) * width) + row];
+
+                if (col > 0)
+                    cells[i].CellToWest = cells[i - 1];
+
+                if (row < height - 1)
+                    cells[i].CellToSouth = cells[((col + 1) * width) + row];
+
+                if (col < width - 1)
+                    cells[i].CellToEast = cells[i + 1];
+            }
+        }
+
+        private void generateDepthFirst()
+        {
+            Stack<Cell> cellStack = new Stack<Cell>();
+            Random random = new Random();
+
+            int visitedCells = 0;
+
+            //  Start the maze at a random position
+            Cell initialCell = cells[random.Next(cells.Count)];
+
+            //  Push the origin onto the stack
+            cellStack.Push(initialCell);
+            visitedCells++;
+
+            //  Repeat until we have visited ever cell in the maze
+            while (visitedCells < cells.Count)
+            {
+                
+                if (cellStack.Peek().NumberOfWalls == 4)
+                {
+                    cellStack.Peek().demolishRandomWall();
+                }
+            }
         }
     }
 }
